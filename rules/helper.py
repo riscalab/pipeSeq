f5a5d2a60cf312d88647dc3491b733ee933c0f52
vriscalab <riscalab@gmail.com>
2019-12-05 16:30:27 -0500

    ready for runtime, i.e. fixed the summary checker

--- rules/helper.py
+++ rules/helper.py
@@ -222,7 +222,7 @@ if __name__ == '__main__':
         if ('Nothing to be done.' in ' '.join(dat)):
             sums+=1
         f.close()
-    if (True):#sums < numSamples):
+    if (sums < numSamples):
         if (not run):
             genomeRef=sys.argv[4]
             blacklist=sys.argv[5]
